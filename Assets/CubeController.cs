b09e79ca7d3e99e76a0614d19d258ab37de4cd1c
omega1224 <84699294+omega1224@users.noreply.github.com>
2021-07-12 21:48:58 +0900

    Initial commit

---
+++ Assets/CubeController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeController : MonoBehaviour
+{
+
+    // キューブの移動速度
+    private float speed = -12;
+
+    // 消滅位置
+    private float deadLine = -10;
+
+    public AudioClip sound;
+    AudioSource audioSource;
+    // Use this for initialization
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // キューブを移動させる
+        transform.Translate(this.speed * Time.deltaTime, 0, 0);
+
+        // 画面外に出たら破棄する
+        if (transform.position.x < this.deadLine)
+        {
+            Destroy(gameObject);
+        }
+
+
+    }
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.tag == "CubeTag" || other.gameObject.tag =="GroundTag")
+        {
+            audioSource.PlayOneShot(sound);
+        }
+
+    }
+
+}
